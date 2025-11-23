c199b386d86e01a33fb1ce87662dc46ff26650c2
Wahyu Saputro Pratjojo <wahyusaputropratjojo@gmail.com>
2025-11-23 19:00:34 +0800

    feat(component): create section two component

---
+++ app/section-two.tsx
@@ -0,0 +1,41 @@
+"use client";
+
+export function SectionTwo() {
+  return (
+    <section className="h-screen px-4 py-12 sm:px-24 relative flex flex-col justify-between">
+      <div className="space-y-4">
+        <p className="copy text-xl font-bold tracking-tight text-white/80">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo, omnis
+          vitae amet laborum commodi error nemo odit velit. Quisquam aperiam
+          ipsum ratione aliquid quaerat ipsa accusantium nostrum aspernatur ut
+          possimus.
+        </p>
+        <p className="copy text-xl font-bold tracking-tight text-white/80">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo, omnis
+          vitae amet laborum commodi error nemo odit velit. Quisquam aperiam
+          ipsum ratione aliquid quaerat ipsa accusantium nostrum aspernatur ut
+          possimus.
+        </p>
+        <p className="copy text-xl font-bold tracking-tight text-white/80">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo, omnis
+          vitae amet laborum commodi error nemo odit velit. Quisquam aperiam
+          ipsum ratione aliquid quaerat ipsa accusantium nostrum aspernatur ut
+          possimus.
+        </p>
+      </div>
+      <div className="space-y-4 uppercase font-bold text-white/80">
+        <div className="flex justify-between items-center">
+          <p className="text-4xl tracking-tight">25</p>
+          <p className="text-4xl rounded-full bg-[#f2efe6] text-[#161c26] px-4 py-2 tracking-tight">
+            Desember
+          </p>
+          <p className="text-4xl tracking-tight">2025</p>
+        </div>
+        <div className="flex justify-between">
+          <p className="tracking-tighter text-6xl">18:00</p>
+          <p className="tracking-tighter text-6xl">WITA.</p>
+        </div>
+      </div>
+    </section>
+  );
+}
