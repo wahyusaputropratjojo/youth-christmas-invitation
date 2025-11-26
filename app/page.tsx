8273d285143656677fc2a46dbf926a9e26488037
Wahyu Saputro Pratjojo <wahyusaputropratjojo@gmail.com>
2025-11-26 20:24:59 +0800

    feat(page): add search param in homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,27 @@
 import { Grainy } from "@/app/grainy";
 import { Footer } from "./footer";
 import { SectionOne } from "./section-one";
+import { SectionThree } from "./section-three";
 import { SectionTwo } from "./section-two";
 
-export default function Home() {
-  return (
-    <main className="overflow-hidden container mx-auto max-w-3xl">
-      <div className="relative">
-        <Grainy />
-        <SectionOne />
-        <SectionTwo />
-      </div>
-      <Footer />
-    </main>
-  );
+export default async function Home({
+	searchParams,
+}: {
+	searchParams: Promise<{ to?: string }>;
+}) {
+	const { to } = await searchParams;
+
+	console.log(to);
+
+	return (
+		<main className="container mx-auto max-w-3xl overflow-hidden">
+			<div className="relative">
+				<Grainy />
+				<SectionOne to={to} />
+				<SectionTwo />
+				<SectionThree />
+			</div>
+			<Footer />
+		</main>
+	);
 }
