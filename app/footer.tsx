985e1267bf776f602fb32c3b753849a57d318c9c
Wahyu Saputro Pratjojo <wahyusaputropratjojo@gmail.com>
2025-11-26 11:14:36 +0800

    feat(component): add profile picture in footer component

--- app/footer.tsx
+++ app/footer.tsx
@@ -1,22 +1,37 @@
 import Image from "next/image";
 import Link from "next/link";
+import Profile from "@/public/profile.webp";
 
 export function Footer() {
-  return (
-    <footer className="font-mono">
-      <Link
-        href="https://www.instagram.com/opos___/"
-        rel="noopener noreferrer"
-        target="_blank"
-      >
-        <div className="space-y-1 p-4">
-          <p className="text-[8px]">Designed and Developed by</p>
-          <div className="flex justify-between font-bold text-xs">
-            <p>Wahyu Saputro Pratjojo</p>
-            <p>@opos___</p>
-          </div>
-        </div>
-      </Link>
-    </footer>
-  );
+	return (
+		<footer>
+			<div className="flex items-center justify-between p-4">
+				<div className="flex items-center gap-2">
+					<Image
+						alt="Wahyu Saputro Pratjojo - Profile"
+						className="size-12 rounded-full border-2 border-blue-500"
+						src={Profile}
+					/>
+					<div>
+						<p className="font-medium text-[8px]">Designed and Developed by</p>
+						<p className="font-bold text-sm">Wahyu Saputro Pratjojo</p>
+						<p className="font-medium text-[8px]">@opos___</p>
+					</div>
+				</div>
+				<div></div>
+				<Link
+					href="https://www.instagram.com/opos___/"
+					rel="noopener noreferrer"
+					target="_blank"
+				>
+					<button
+						className="h-8 cursor-pointer rounded-full bg-blue-500 px-3 font-bold text-sm"
+						type="button"
+					>
+						Follow
+					</button>
+				</Link>
+			</div>
+		</footer>
+	);
 }
