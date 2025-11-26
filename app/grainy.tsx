a4aa5481ea12dd18e8b756829a30fa01fa662502
Wahyu Saputro Pratjojo <wahyusaputropratjojo@gmail.com>
2025-11-26 20:28:41 +0800

    refactor: update code style using new biome configuration

--- app/grainy.tsx
+++ app/grainy.tsx
@@ -1,16 +1,16 @@
 export function Grainy() {
-  return (
-    <svg className="size-full absolute z-50">
-      <title>Grain</title>
-      <filter id="filter">
-        <feTurbulence baseFrequency={0.7} type="turbulence" />
-      </filter>
-      <rect
-        className="opacity-20"
-        filter="url(#filter)"
-        height="100%"
-        width="100%"
-      />
-    </svg>
-  );
+	return (
+		<svg className="absolute z-50 size-full">
+			<title>Grain</title>
+			<filter id="filter">
+				<feTurbulence baseFrequency={0.7} type="turbulence" />
+			</filter>
+			<rect
+				className="opacity-20"
+				filter="url(#filter)"
+				height="100%"
+				width="100%"
+			/>
+		</svg>
+	);
 }
