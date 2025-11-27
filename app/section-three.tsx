9eba875e88bbe7da0d1b7b8fcce3ab670793d4bb
Wahyu Saputro Pratjojo <wahyusaputropratjojo@gmail.com>
2025-11-28 00:30:06 +0800

    feat(component): update section three component

--- app/section-three.tsx
+++ app/section-three.tsx
@@ -1,33 +1,75 @@
 "use client";
 
+import Image from "next/image";
+import Gereja from "@/public/gereja.webp";
+
 export function SectionThree() {
 	return (
 		<section className="relative flex flex-col justify-between overflow-hidden text-pretty bg-[#234940] px-4 py-8 font-medium text-[#f2efe6] sm:px-24">
-			<div className="space-y-4">
-				<p className="text-lg">
+			<div className="space-y-8">
+				<div className="space-y-2 rounded-3xl bg-[#f2efe6] p-8 text-[#234940]">
+					<p className="w-min rounded-xl bg-[#234940] px-4 font-bold text-[#f2efe6] text-xl italic tracking-tight sm:text-3xl">
+						Tema
+					</p>
+					<p className="font-bold text-xl italic tracking-tight sm:text-3xl">
+						Rise and Shine: Be The Light
+					</p>
+					<p className="text-xs sm:text-sm">
+						"Demikianlah hendaknya terangmu bercahaya di depan orang, supaya
+						mereka melihat perbuatanmu yang baik dan memuliakan Bapamu yang di
+						surga." - Matius 5 : 16
+					</p>
+				</div>
+				<p className="font-semibold text-xl tracking-tight first-letter:float-left first-letter:mr-3 first-letter:text-5xl first-letter:italic sm:text-3xl sm:first-letter:mr-6 sm:first-letter:text-8xl">
 					Melalui undangan ini, kami Komisi Pemuda GMIM Smirna Batulubang,
 					mengundang teman - teman untuk dapat hadir dalam satu persekutuan
-					Ibadah Natal Pemuda GMIM Smirna Batulubang. Yang akan dilaksanakan
-					pada
+					Ibadah,
 				</p>
-				<div className="grid gap-2">
-					<div>
-						<p className="text-lg">Tanggal</p>
-						<p className="font-black text-3xl">25 DESEMBER 2025</p>
+				<p className="font-semibold text-xl tracking-tight sm:text-3xl">
+					Perayaan Sambut Natal Pemuda GMIM Smirna Batulubang dan Pemuda Wilayah
+					Bitung IV.
+				</p>
+				<p className="font-semibold text-xl tracking-tight sm:text-3xl">
+					Yang akan dilaksanakan pada,
+				</p>
+				<div>
+					<div className="flex items-center gap-2 rounded-full bg-[#f2efe6] p-2 text-[#234940]">
+						<p className="flex-1 rounded-full bg-[#234940] p-4 text-center font-black text-4xl text-[#f2efe6] uppercase">
+							Jumat
+						</p>
 					</div>
-					<div>
-						<p className="text-lg">Jam</p>
-						<p className="font-black text-3xl">18:00 WITA</p>
+					<div className="flex items-center gap-2 rounded-full bg-[#f2efe6] p-2 text-[#234940]">
+						<p className="rounded-full bg-[#234940] p-4 text-center font-black text-4xl text-[#f2efe6] uppercase">
+							14
+						</p>
+						<p className="flex-1 rounded-full bg-[#234940] p-4 text-center font-black text-4xl text-[#f2efe6] uppercase">
+							Desember
+						</p>
 					</div>
-					<div>
-						<p className="text-lg">Lokasi</p>
-						<p className="font-black text-3xl">Gereja GMIM Smirna Batulubang</p>
+					<div className="flex items-center gap-2 rounded-full bg-[#f2efe6] p-2 text-[#234940]">
+						<p className="flex-1 rounded-full bg-[#234940] p-4 text-center font-black text-4xl text-[#f2efe6] uppercase">
+							16:00
+						</p>
+						<p className="rounded-full bg-[#234940] p-4 text-center font-black text-4xl text-[#f2efe6] uppercase">
+							Wita
+						</p>
 					</div>
 				</div>
-				<p className="text-lg">
-					Besar harapan kami agar teman - teman dapat meringankan langkah untuk
-					hadir bersekutu bersama. Atas perhatiannya, kami ucapkan terima kasih.
-				</p>
+				<div className="grid gap-8">
+					<div>
+						<p className="text-right font-semibold text-xl tracking-tight sm:text-3xl">
+							Lokasi
+						</p>
+						<p className="text-right font-black text-4xl uppercase">
+							Gereja GMIM Smirna Batulubang
+						</p>
+						<Image
+							alt="Gereja GMIM Smirna Batulubang"
+							className="scale-120"
+							src={Gereja}
+						/>
+					</div>
+				</div>
 			</div>
 		</section>
 	);
