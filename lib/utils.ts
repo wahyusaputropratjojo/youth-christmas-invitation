df63daedd870f5d9638655bbb2e88e663947a046
Wahyu Saputro Pratjojo <wahyusaputropratjojo@gmail.com>
2025-11-28 13:50:02 +0800

    feat(lib): create utils function

---
+++ lib/utils.ts
@@ -0,0 +1,22 @@
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
+
+export function cn(...inputs: ClassValue[]) {
+	return twMerge(clsx(inputs));
+}
+
+export function getBaseUrl() {
+	if (typeof window !== "undefined") {
+		return window.location.origin;
+	}
+
+	if (process.env.NEXT_PUBLIC_SITE_URL) {
+		return process.env.NEXT_PUBLIC_SITE_URL;
+	}
+
+	if (process.env.VERCEL_URL) {
+		return `https://${process.env.VERCEL_URL}`;
+	}
+
+	return "http://localhost:3000";
+}
